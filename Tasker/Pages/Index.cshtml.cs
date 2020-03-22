b650dd7d6b7aaed68812636ba3fbc6337b31ae90
Gill Cleeren <gill@snowball.be>
2020-03-22 17:36:01 +0100

    Commit 4

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -9,8 +9,20 @@ namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+
+        private ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Task> Tasks { get; set; }
+
+
         public void OnGet()
         {
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
